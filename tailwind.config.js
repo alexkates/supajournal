5cec4733523f4fd8b7f3840c8b8a47baac0e3692
Alex Kates <alex.kates@croissant.com>
2023-12-09 06:08:50 -0500

    clean up boiler plate

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        btn: {
-          background: 'hsl(var(--btn-background))',
-          'background-hover': 'hsl(var(--btn-background-hover))',
-        },
-      },
-    },
+
   },
   plugins: [],
 }
