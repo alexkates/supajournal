9cced6ad3eaa4c793f2b5ae25504180eda6e19b3
Alex Kates <alex.kates@croissant.com>
2023-12-17 09:13:32 -0500

    Refactor calculateStreak function to improve performance and readability

--- lib/get-streak-stats.ts
+++ lib/get-streak-stats.ts
@@ -1,13 +1,38 @@
-export default function calculateStreak(data: { createdAt: string; wordCount: number }[]) {
-  data.sort((a, b) => b.createdAt.localeCompare(a.createdAt));
+type JournalEntry = {
+  createdAt: string;
+  wordCount: number;
+};
 
+export default function calculateStreak(entries: JournalEntry[]) {
   let streak = 0;
   let currentDate = new Date();
 
-  for (const entry of data) {
-    const entryDate = new Date(entry.createdAt);
+  const entriesDistinctByCreatedAt = entries
+    .filter((entry, index, self) => {
+      const entryCreatedAtDate = new Date(entry.createdAt);
+      const entryCreatedAtDateString = entryCreatedAtDate.toDateString();
 
-    if (entryDate.toDateString() === currentDate.toDateString()) {
+      return (
+        index ===
+        self.findIndex((e) => {
+          const eCreatedAtDate = new Date(e.createdAt);
+          const eCreatedAtDateString = eCreatedAtDate.toDateString();
+
+          return eCreatedAtDateString === entryCreatedAtDateString;
+        })
+      );
+    })
+    .sort((a, b) => {
+      const aCreatedAtDate = new Date(a.createdAt);
+      const bCreatedAtDate = new Date(b.createdAt);
+
+      return bCreatedAtDate.getTime() - aCreatedAtDate.getTime();
+    });
+
+  for (const entry of entriesDistinctByCreatedAt) {
+    const entryCreatedAtDate = new Date(entry.createdAt);
+
+    if (entryCreatedAtDate.toDateString() === currentDate.toDateString()) {
       if (entry.wordCount > 0) {
         streak++;
       } else {
