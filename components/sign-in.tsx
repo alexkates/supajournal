d5b7bf43b9a5d705489ee84ed20f5a8ea3cb377c
Alex Kates <alex.kates@croissant.com>
2023-12-17 15:26:54 -0500

    Update redirect URL in auth callback and sign-in component

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -14,11 +14,23 @@ export default function SignIn() {
 
   const supabase = createClientComponentClient();
 
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+  };
+
   const handleSignIn = async () => {
     const response = await supabase.auth.signInWithOtp({
       email,
       options: {
-        emailRedirectTo: `${location.origin}/auth/callback`,
+        emailRedirectTo: `${getURL()}/auth/callback`,
       },
     });
 
